38fa9fb51420aa4823da97e3e37308f988a90b1d
jondendy <81487636+jondendy@users.noreply.github.com>
2025-01-24 13:43:29 +0000

    graphice version uploaded
    .env file removed

--- flask_app/enhanced_dynamic_calculator_with_graphics.py
+++ flask_app/enhanced_dynamic_calculator_with_graphics.py
@@ -1,8 +1,9 @@
-""" module importing streamlit """
+''' streamlit_code = '''
 import streamlit as st
+from PIL import Image, ImageDraw
 
 # Title of the app
-st.title('Enhanced Dynamic Calculator')
+st.title('Enhanced Dynamic Calculator with Graphics')
 
 # Number of variables
 num_vars = st.slider('Enter the number of variables needed', 2, 6, 2)
@@ -24,6 +25,22 @@ num_children = st.number_input('Number of children in household', min_value=0, v
 # Home size factor
 home_type = st.selectbox('Type of home', ('Detached', 'Bungalow', 'Semi-detached', 'Flat'))
 num_bedrooms = st.number_input('Number of bedrooms', min_value=1, value=1)
+
+# Draw stick family
+family_image = Image.new('RGB', (200, 100), color = (255, 255, 255))
+draw = ImageDraw.Draw(family_image)
+for i in range(num_adults):
+    draw.ellipse((10 + i*30, 10, 30 + i*30, 30), fill=(0, 0, 0))
+for i in range(num_children):
+    draw.ellipse((10 + (num_adults + i)*30, 40, 30 + (num_adults + i)*30, 60), fill=(0, 0, 0))
+st.image(family_image, caption='Family Representation')
+
+# Draw home
+home_image = Image.new('RGB', (200, 100), color = (255, 255, 255))
+draw = ImageDraw.Draw(home_image)
+draw.rectangle((50, 50, 150, 100), outline=(0, 0, 0))
+draw.polygon([(50, 50), (100, 20), (150, 50)], outline=(0, 0, 0))
+st.image(home_image, caption='Home Representation')
 
 # Perform calculation with BODMAS and scaling
 try:
@@ -47,7 +64,14 @@ try:
     result *= home_factor[home_type] * num_bedrooms
 
     st.success(f'Result: {result}')
-except ZeroDivisionError as e:
-    st.error(f'Error: Division by zero {str(e)}')
-except TypeError as e:
-    st.error(f'Error: Invalid input type {str(e)}')
+except ZeroDivisionError:
+        st.error("Error: Division by zero")
+except TypeError:
+        st.error("Error: Invalid input type")
+
+# Information text box
+st.text_area('Calculator Information', 'This calculator helps you estimate energy consumption...\n \n Some calculations to try are...')
+'''
+
+with open('enhanced_dynamic_calculator_with_graphics.py', 'w') as f:
+    f.write(streamlit_code)
